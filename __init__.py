229c0619ccab90de694c6a1bb5392118c88b88df
Manos S.H <manossef@gmail.com>
2019-10-15 15:42:45 +0100

    Patch REST framework only during or after configuration

--- __init__.py
+++ __init__.py
@@ -1,2 +1,17 @@
+import sys
+
+from django.core.exceptions import ImproperlyConfigured
+
 from .roles import is_creator, is_user, is_anon, is_admin, is_staff
-from . import patching
+
+
+def _patch_rest_framework():
+    from .views import PatchedAPIView
+    sys.modules['rest_framework'].views.APIView = PatchedAPIView
+
+
+# Ensure patching happens during the configuration of Django or after
+try:
+    _patch_rest_framework()
+except ImproperlyConfigured:
+    pass
--- patching.py
+++
@@ -1,5 +0,0 @@
-import sys
-from .views import PatchedAPIView
-
-# We replace the Django REST view with our patched one
-sys.modules['rest_framework'].views.APIView = PatchedAPIView
