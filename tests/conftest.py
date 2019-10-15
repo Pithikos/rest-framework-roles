b099d3e435f0f1e78f66f3941dd322d65c11a7cd
Manos S.H <manossef@gmail.com>
2019-10-15 15:44:14 +0100

    Add conftest

---
+++ tests/conftest.py
@@ -0,0 +1,50 @@
+
+import django
+from django.conf import settings
+
+
+def pytest_configure(config):
+    settings.configure(
+        DEBUG_PROPAGATE_EXCEPTIONS=True,
+        DATABASES={
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': ':memory:'
+            }
+        },
+        SITE_ID=1,
+        SECRET_KEY='not very secret in tests',
+        USE_I18N=True,
+        USE_L10N=True,
+        STATIC_URL='/static/',
+        ROOT_URLCONF='tests.urls',
+        TEMPLATES=[
+            {
+                'BACKEND': 'django.template.backends.django.DjangoTemplates',
+                'APP_DIRS': True,
+                'OPTIONS': {
+                    "debug": True,  # We want template errors to raise
+                }
+            },
+        ],
+        MIDDLEWARE=(
+            'django.middleware.common.CommonMiddleware',
+            'django.contrib.sessions.middleware.SessionMiddleware',
+            'django.contrib.auth.middleware.AuthenticationMiddleware',
+            'django.contrib.messages.middleware.MessageMiddleware',
+        ),
+        INSTALLED_APPS=(
+            'django.contrib.admin',
+            'django.contrib.auth',
+            'django.contrib.contenttypes',
+            'django.contrib.sessions',
+            'django.contrib.sites',
+            'django.contrib.staticfiles',
+            'rest_framework',
+            'rest_framework_roles',
+        ),
+        PASSWORD_HASHERS=(
+            'django.contrib.auth.hashers.MD5PasswordHasher',
+        ),
+    )
+    django.setup()
