0b658dc01372ef73f8d7399f8bfc82105454edf1
Manos S.H <manossef@gmail.com>
2019-10-16 18:22:48 +0100

    Add test_permissions

--- tests/test_permissions.py
+++ tests/test_permissions.py
@@ -14,6 +14,7 @@ from rest_framework import serializers, permissions
 
 from ..roles import is_admin, is_user, is_anon
 from ..permissions import is_self
+from .utils import assert_allowed, assert_disallowed
 # from ..parsing import create_lookup
 
 """
@@ -43,11 +44,6 @@ def test_view_permissions_can_be_applied_at_settings():
     pass
 
 
-BAD_REQUEST = 400
-FORBIDDEN = 403
-ALLOWED = 200
-
-
 class TestUserAPI():
 
     def setUp(self):
@@ -72,17 +68,20 @@ class TestUserAPI():
         }]
         # TODO: Actually load the above in our system
 
-    def test_only_admin_can_list_users(self, client, user, admin):
-        client.force_login(admin)
-        request = client.get('/users/')
-        assert request.status_code == ALLOWED
-        client.force_login(user)
-        request = client.get('/users/')
-        assert request.status_code == FORBIDDEN
-        # anon
-        request = client.get('/users/')
-        assert request.status_code == FORBIDDEN
+    def test_only_admin_can_list_users(self, client, user, anon, admin):
+        assert_allowed(admin, get='/users/')
+        assert_disallowed(user, get='/users/')
+        assert_disallowed(anon, get='/users/')
 
+    def test_user_can_retrieve_only_self(self, client, user, anon, admin):
+        other_user = User.objects.create(username='mrother')
+        other_user_url = f'/users/{other_user.id}/'
+        all_users_url = '/users/'
+        assert_allowed(admin, get=other_user_url)
+        assert_disallowed(user, get=other_user_url)
+        assert_disallowed(anon, get=other_user_url)
+        assert_allowed(admin, get=f'/users/{admin.id}/')
+        assert_allowed(user, get=f'/users/{user.id}/')
 
 def test_view_redirectios_dont_omit_checks():
     pass
