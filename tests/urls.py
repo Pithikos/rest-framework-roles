a9c54e7fac755f662a8b96c73f91019cf21d00fc
Manos S.H <manossef@gmail.com>
2019-10-16 16:57:00 +0100

    Add urls for tests

---
+++ tests/urls.py
@@ -0,0 +1,10 @@
+from rest_framework import routers
+from .views import UserViewSet
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
