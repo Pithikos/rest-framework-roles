67cac2da21fcd15695f413e0578c5c5655d068fc
Manos S.H <manossef@gmail.com>
2019-10-21 17:25:27 +0100

    Cleanup patching code

--- patching.py
+++ patching.py
@@ -1,6 +1,8 @@
 import sys
+import importlib
 
 from django.urls import resolve, get_resolver
+from django.urls.resolvers import URLPattern
 from django.conf import settings
 from django.utils.functional import empty
 
@@ -13,11 +15,25 @@ def is_rest_framework_loaded():
     return 'rest_framework' in sys.modules.keys()
 
 
-def patch():
-    # Ensure patching happens during the configuration of Django or after
-    if is_django_configured():
-        from .views import PatchedAPIView
-        sys.modules['rest_framework'].views.APIView = PatchedAPIView
+def patch(urlconf=None):
+    """
+    Entrypoint for all patching (after configurations have loaded)
+
+    Args:
+        urlconf(str): Path to urlconf, by default using ROOT_URLCONF
+    """
+    def before_view(target_view):
+        def wrapped(request, *args, **kwargs):
+            print('BEFORE VIEW..')
+            import IPython; IPython.embed(using=False)
+            return target_view(request, *args, **kwargs)
+        return wrapped
+
+    # Monkey-patch each view
+    if not urlconf:
+        urlconf = importlib.import_module(settings.ROOT_URLCONF)
+    for pattern in iter_urlpatterns(urlconf.urlpatterns):
+        pattern.callback = before_view(pattern.callback)
 
 
 def get_active_views():
@@ -30,3 +46,22 @@ def get_active_views():
         else:
             raise Exception('View must be callable or string')
     return views
+
+
+def iter_urlpatterns(urlpatterns):
+    for entity in urlpatterns:
+        if hasattr(entity, 'url_patterns'):
+            yield from iter_urlpatterns(entity.url_patterns)
+        elif hasattr(entity, 'urlpatterns'):
+            yield from iter_urlpatterns(entity.urlpatterns)
+        else:
+            assert type(entity) == URLPattern, f"Expected pattern, got '{entity}'"
+            yield entity
+
+
+def extract_views_from_urlpatterns(urlpatterns):
+    """
+    Similar to iter_urlpatterns but uses django-extensions' show_urls methodology
+    """
+    from django_extensions.management.commands.show_urls import Command
+    return Command().extract_views_from_urlpatterns(urlpatterns)
