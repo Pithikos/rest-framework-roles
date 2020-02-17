1af9359f1ac90aed6d53990eeb96e6a6b890f24c
Manos S.H <manossef@gmail.com>
2020-02-17 15:01:29 +0000

    Add setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='rest_framework_roles',
+    version='0.2.0',
+    author='Johan Hanssen Seferidis',
+    author_email='manossef@gmail.com',
+    packages=['rest_framework_roles'],
+    url='https://pypi.org/project/rest-framework-roles/',
+    license='LICENSE',
+    description='Role-based permissions for Django REST Framework and vanilla Django.',
+    long_description=open('README.md').read(),
+    install_requires=[],
+)
