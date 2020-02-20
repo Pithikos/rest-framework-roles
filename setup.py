d75b7515c52aa8a4226a4e889d7c493d60780f3a
Manos S.H <manossef@gmail.com>
2020-02-20 08:11:51 +0000

    Add command verify to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,28 @@
-from distutils.core import setup
+import os
+import sys
+
+try:
+    from setuptools import setup
+    from setuptools.command.install import install
+except ImportError:
+    from distutils.core import setup
+    from distutils.command.install import install
+
 
 VERSION = '0.2.0'
+
+
+class VerifyVersionCommand(install):
+    """ Custom command to verify that the git tag matches our version """
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+        if not tag:
+            sys.exit("Missing environment variable 'CIRCLE_TAG'")
+        if tag != VERSION:
+            sys.exit(f"Git tag: {tag} does not match the version of this app: {VERSION}")
+
 
 setup(
     name='rest_framework_roles',
@@ -13,6 +35,7 @@ setup(
     license='LICENSE',
     long_description=open('README.md').read(),
     install_requires=[],
+    python_requires='>=3',
     keywords=[
         'permissions',
         'roles',
@@ -21,4 +44,7 @@ setup(
         'Framework :: Django',
         'Topic :: Security',
     ],
+    cmdclass={
+        'verify': VerifyVersionCommand,
+    },
 )
