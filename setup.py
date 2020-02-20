46622df2a1c5e8e96c0ccc1f9c0492bcbdc59e52
Manos S.H <manossef@gmail.com>
2020-02-20 08:06:10 +0000

    Add package meta info

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
 from distutils.core import setup
 
+VERSION = '0.2.0'
+
 setup(
     name='rest_framework_roles',
-    version='0.2.0',
+    version=VERSION,
+    description='Role-based permissions for Django REST Framework and vanilla Django.',
     author='Johan Hanssen Seferidis',
     author_email='manossef@gmail.com',
     packages=['rest_framework_roles'],
     url='https://pypi.org/project/rest-framework-roles/',
     license='LICENSE',
-    description='Role-based permissions for Django REST Framework and vanilla Django.',
     long_description=open('README.md').read(),
     install_requires=[],
+    keywords=[
+        'permissions',
+        'roles',
+    ],
+    classifiers=[
+        'Framework :: Django',
+        'Topic :: Security',
+    ],
 )
