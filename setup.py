dec1510f7a00e99552a2068630fea9eb087fc500
Manos S.H <manossef@gmail.com>
2020-02-20 10:42:00 +0000

    Use git to get version tag instead of relying on CIRCLE_TAG

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 import os
 import sys
+import re
+import subprocess
+import shlex
 
 try:
     from setuptools import setup
@@ -12,15 +15,23 @@ except ImportError:
 VERSION = '0.2.2'
 
 
+def get_tag_version():
+    cmd = 'git tag --points-at HEAD'
+    versions = subprocess.check_output(shlex.split(cmd)).splitlines()
+    if len(versions) != 1:
+        sys.exit(f"Trying to get tag via git: Expected excactly one tag, got {len(versions)}")
+    version = versions[0].decode()
+    if re.match('^v[0-9]', version):
+        version = version[1:]
+    return version
+
+
 class VerifyVersionCommand(install):
     """ Custom command to verify that the git tag matches our version """
     description = 'verify that the git tag matches our version'
 
     def run(self):
-        tag = os.getenv('CIRCLE_TAG')
-        if not tag:
-            sys.exit("Missing environment variable 'CIRCLE_TAG'")
-        if tag != VERSION:
+        if get_tag_version() != VERSION:
             sys.exit(f"Git tag: {tag} does not match the version of this app: {VERSION}")
 
 
