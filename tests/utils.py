55153fdbe0837b6e5fb16b6f5bed9e63933454eb
Manos S.H <manossef@gmail.com>
2020-02-28 11:09:51 +0000

    Add is_predispatch_patched

--- tests/utils.py
+++ tests/utils.py
@@ -48,6 +48,10 @@ def is_patched(fn):
     return 'before_view' in str(fn)
 
 
+def is_predispatch_patched(fn):
+    return 'before_dispatch' in str(fn)
+
+
 def has_view_permissions(fn):
     return hasattr(fn, 'view_permissions')
 
