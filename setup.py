04a1941a5397363b42c445b92c1cff438c7d1093
Manos <manossef@gmail.com>
2024-02-01 19:08:30 +0100

    Update homepage URL

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     author='Johan Hanssen Seferidis',
     author_email='manossef@gmail.com',
     packages=['rest_framework_roles'],
-    url='https://pypi.org/project/rest-framework-roles/',
+    url='https://github.com/Pithikos/rest-framework-roles',
     license='LICENSE',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
