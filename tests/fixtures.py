8e3c4c9052d372d9cb7d30b976e8d095c6b6976c
Manos <manossef@gmail.com>
2025-05-21 13:17:36 +0300

    Add test users

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -7,13 +7,11 @@ from django.contrib.auth.models import User, AnonymousUser
 def anon(db):
     return AnonymousUser()
 
-
 @fixture
 def user(db):
     return User.objects.create(
         username='mruser',
     )
-
 
 @fixture
 def admin(db):
@@ -22,7 +20,24 @@ def admin(db):
         is_staff=True,
         is_superuser=True,
     )
+    
+@fixture
+def test_user1(db):
+    return User.objects.create(
+        username='test_user1',
+    )
 
+@fixture
+def test_user2(db):
+    return User.objects.create(
+        username='test_user2',
+    )
+
+@fixture
+def test_user3(db):
+    return User.objects.create(
+        username='test_user3',
+    )
 
 @fixture(scope='function')
 def request_factory():
